3cff9849f794891f7137e6fa31edb62f56e6317b
Chinmay Tagare <t.chinmaybhalchandra@gmail.com>
2021-06-06 11:15:25 +0530

    Experimented few things on data frames- added as comments

--- Src/Intro_data.py
+++ Src/Intro_data.py
@@ -1,7 +1,18 @@
 # Author       : Chinmay Tagare
-# Last Updated : 05-06-2021
+# Last Updated : 06-June-2021
 
 import pandas as pd
 
-df = pd.read_csv("D:\\Chinmay\\GitHub\\Data-Analysis-with-Pandas\\Data-Analysis-with-Pandas\\Data_Sets\\avocado.csv")
-print(df)
+data_path = "D:\\Chinmay\\GitHub\\Data-Analysis-with-Pandas\\Data-Analysis-with-Pandas\\Data_Sets\\avocado.csv"
+df = pd.read_csv(data_path)
+# =============================================================================
+# print(df.head(3))
+# print(df.tail(2))
+# for col in df.columns:
+#     print(col)
+# print(df["AveragePrice"])
+# print(df["AveragePrice"].head())
+# =============================================================================
+
+albany_df = df[ df["region"]=='Albany' ]
+print(albany_df.head())
